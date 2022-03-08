9b747c4e9611e20592d30cd15d844505d5726071
Joel Anderson <joelanderson333@gmail.com>
2022-03-07 21:22:41 -0500

    add journald and wel features

--- build.rs
+++ build.rs
@@ -1,27 +1,38 @@
-extern crate bindgen;
+use bindgen;
 
 fn main() {
+    // enabling requested features
+    let journald_enabled = if cfg!(feature = "journald") {
+        "ON"
+    } else {
+        "OFF"
+    };
+
+    let wel_enabled = if cfg!(feature = "wel") {
+        "ON"
+    } else {
+        "OFF"
+    };
+
     let stumpless_out = cmake::Config::new("stumpless")
         .define("BUILD_SHARED_LIBS", "OFF")
+        .define("ENABLE_JOURNALD_TARGETS", journald_enabled)
+        .define("ENABLE_WINDOWS_EVENT_LOG_TARGETS", wel_enabled)
         .build();
+
     println!(
         "cargo:rustc-link-search=native={}/lib",
         stumpless_out.display()
     );
     println!("cargo:rustc-link-lib=stumpless");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
+    let bindings_builder = bindgen::Builder::default()
         .header(format!("{}/include/stumpless.h", stumpless_out.display()))
         .clang_arg(format!("-I{}/include/", stumpless_out.display()))
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks));
+
+    let bindings = bindings_builder
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     bindings
