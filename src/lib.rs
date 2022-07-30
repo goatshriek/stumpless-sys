ee02deb25294bd4e690a3609420a01fc59fc7f30
Joel Anderson <joelanderson333@gmail.com>
2022-07-30 14:24:00 -0400

    test function to dump resources

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,12 @@
 #![allow(non_snake_case)]
 
 include!("bindings.rs");
+
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn write_message_resource_file(path: &str) -> std::io::Result<()> {
+    let mut file = File::create(&path)?;
+    file.write_all(b"hello!")?;
+    Ok(())
+}
