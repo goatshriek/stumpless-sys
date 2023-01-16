693920396591e9e85b2f73a1c3f0702d48d0176e
Joel Anderson <joelanderson333@gmail.com>
2023-01-16 14:34:49 -0500

    add documentation

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+All notable changes to stumpless will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+and this project adheres to
+[Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+
+## [0.2.0] - 2023-01-16
+### Changed
+ - The `network` features is now on by default.
+
+
+## [0.1.0] - 2023-01-06
+### Added
+ - FFI bindings for
+   [stumpless@5c2f299](https://github.com/goatshriek/stumpless/tree/5c2f29981e9be00e793fac064c4c7c5172f662f2)
+ - `journald`, `network`, `socket`, and `wel` features based on the target types
+   within stumpless.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! FFI bindings for the Stumpless logging library.
+//!
+//! These wrappings are intended to be the bare minimum to use Stumpless from
+//! a Rust application.
+//!
+//!
+//! # Create Features
+//! Stumpless provides a number of build configuration options that can be used
+//! to enable or disable different functionality. This crate exposes these
+//! options as the following features.
+//!
+//!
+//! ### Target Features
+//!
+//! * **journald** -
+//!   Enables targets that can send logs to a systemd journald daemon.
+//! * **network** -
+//!   Enables targets that can send logs to a server over a network connection.
+//! * **socket** -
+//!   Enables targets that can send logs to Unix sockets.
+//! * **wel** -
+//!   Enables targets that can send logs to the Windows Event Log.
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
@@ -11,6 +34,7 @@ use std::io::Write;
 #[cfg(feature = "wel")]
 use std::path::Path;
 
+/// Writes a compiled resource file (.bin) to the provided file.
 #[cfg(feature = "wel")]
 pub fn write_default_events_bin_file<P: AsRef<Path>>(path: &P) -> std::io::Result<()> {
     let resource_bytes = include_bytes!(env!("STUMPLESS_DEFAULT_EVENTS_BIN_PATH"));
@@ -19,6 +43,8 @@ pub fn write_default_events_bin_file<P: AsRef<Path>>(path: &P) -> std::io::Resul
     Ok(())
 }
 
+/// Writes the resource definition (.rc) file for the default Stumpless events
+/// to the provided file.
 #[cfg(feature = "wel")]
 pub fn write_default_events_resource_file<P: AsRef<Path>>(path: &P) -> std::io::Result<()> {
     let resource_bytes = include_bytes!(env!("STUMPLESS_DEFAULT_EVENTS_RC_PATH"));
